74a853264d4bbb31d31947c7d6a51a57d59e4f3d
Liquelaliqour <lewismomanyi34@gmail.com>
2025-09-28 23:06:12 +0300

    0. Implementing a share layout 1. Importing Google fonts 2. Setting up our landing page with Imperative routing 3. Move your interfaces to the right directory 4. Resolve a 404 page not found

---
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -0,0 +1,21 @@
+
+
+import Footer from "./Footer";
+import Header from "./Header";
+import { ReactNode } from "react";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
