05b00c97deba5f7764c6249ab0ab176fa3b4d7ef
Liquelaliqour <lewismomanyi34@gmail.com>
2025-09-28 23:29:49 +0300

    Quick Fix

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -13,8 +13,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
-        {/* 👇 literal match for the grader */}
-        <span> Welcome to Splash App </span>
+        Welcome to Splash App
       </h1>
 
       <p className="text-lg text-gray-600 mb-8">
